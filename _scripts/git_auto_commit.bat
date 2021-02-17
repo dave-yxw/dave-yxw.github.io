b0a1083eec1407ce660526327ae1c098c0f20f71
dave-yxw <dave.yxw@gmail.com>
2021-02-18 06:10:34 +0800

    daily report

--- _scripts/git_auto_commit.bat
+++ _scripts/git_auto_commit.bat
@@ -1,7 +1,11 @@
 cd /d %~dp0
+cd ..
+
+python _scripts/daily_build.py
 
 git pull
 git add mt_report _data
+
 git commit -m "daily report"
 git push origin master
 
--- _scripts/git_auto_commit.sh
+++ _scripts/git_auto_commit.sh
@@ -2,7 +2,9 @@ cd `dirname "$0"`
 cd ..
 
 git pull
+python _scripts/daily_build.py
 git add mt_report _data
+
 git commit -m "daily report"
 git push origin master
 
